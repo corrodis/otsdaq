479e16b0656f62eaaab4902bee023ed2fc0b71c3
rrivera <rrivera@fnal.gov>
2017-07-07 11:53:03 -0500

    minor fix to flatten system aliases for handling OTS_MAIN_PORT in configurations

--- test/ConfigurationInterface/otsdaq_flatten_system_aliases.cc
+++ test/ConfigurationInterface/otsdaq_flatten_system_aliases.cc
@@ -85,6 +85,9 @@ void FlattenActiveSystemAliasConfigurationGroups(int argc, char* argv[])
 	setenv("OTSDAQ_LIB",(std::string(getenv("USER_DATA")) + "/").c_str(),1);
 	setenv("OTSDAQ_UTILITIES_LIB",(std::string(getenv("USER_DATA")) + "/").c_str(),1);
 
+	//Some configuration plug-ins use getenv("OTS_MAIN_PORT") in init() so define it
+	setenv("OTS_MAIN_PORT","2015",1);
+
 	//also xdaq envs for XDAQContextConfiguration
 	setenv("XDAQ_CONFIGURATION_DATA_PATH",(std::string(getenv("USER_DATA")) + "/XDAQConfigurations").c_str(),1);
 	setenv("XDAQ_CONFIGURATION_XML","otsConfigurationNoRU_CMake",1);
