faf8e965d36abe545f353ebf1a49134327e20787
Lorenzo Uplegger <uplegger@fnal.gov>
2018-04-06 13:54:07 -0500

    Fixed MessageFacility.h, I think

--- otsdaq-core/MessageFacility/MessageFacility.h
+++ otsdaq-core/MessageFacility/MessageFacility.h
@@ -3,27 +3,36 @@
 
 #include <messagefacility/MessageLogger/MessageLogger.h>
 #include "artdaq-core/Utilities/configureMessageFacility.hh"
+#include "otsdaq-core/Macros/CoutHeaderMacros.h"
 
-namespace ots {
+namespace ots
+{
 
 inline void INIT_MF(const char* name)
 {
 	char* logRootString = getenv("OTSDAQ_LOG_ROOT");
-	char* logFhiclCode = getenv("OTSDAQ_LOG_FHICL");
+	if (logRootString == nullptr)
+	{
 
-	if (logRootString != nullptr)
-	{
+		__COUT_ERR__ << "\n**********************************************************" << std::endl;
+		__COUT_ERR__ << "WARNING: OTSDAQ_LOG_ROOT environment variable was not set!" << std::endl;
+		__COUT_ERR__ << "**********************************************************\n" << std::endl;;
+		//exit(0);
+	}
+	else
 		setenv("ARTDAQ_LOG_ROOT", logRootString, 1);
-	}
-	if (logFhiclCode != nullptr)
+
+	char* logFhiclCode  = getenv("OTSDAQ_LOG_FHICL");
+	if (logFhiclCode == nullptr)
 	{
-		char* alf = getenv("ARTDAQ_LOG_FHICL");
-		if (alf != nullptr)
-		{
-			logFhiclCode = strcat(logFhiclCode, alf);
-		}
-		setenv("ARTDAQ_LOG_FHICL", logFhiclCode, 1);
+		__COUT_ERR__ << "\n***********************************************************" << std::endl;
+		__COUT_ERR__ << "WARNING: OTSDAQ_LOG_FHICL environment variable was not set!" << std::endl;
+		__COUT_ERR__ << "***********************************************************\n" << std::endl;
+		//exit(0);
 	}
+	else
+		setenv("ARTDAQ_LOG_FHICL", logFhiclCode, 1);
+
 	artdaq::configureMessageFacility(name,true,true);
 	artdaq::setMsgFacAppName(name,0);
 }
