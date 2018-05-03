de3c2ca3ab56839197d27eaf59cf2be6e24ccd95
Eric Flumerfelt <eflumerf@fnal.gov>
2018-05-03 15:23:55 -0500

    Define OTSDAQ_LOG_ROOT if OTSDAQ_LOG_DIR is defined

--- tools/StartOTS.sh
+++ tools/StartOTS.sh
@@ -316,6 +316,7 @@ fi
 
 if [ ! -d $OTSDAQ_LOG_DIR ]; then
     mkdir -p $OTSDAQ_LOG_DIR
+	export OTSDAQ_LOG_ROOT=$OTSDAQ_LOG_DIR
 fi
 
 ##############################################################################
