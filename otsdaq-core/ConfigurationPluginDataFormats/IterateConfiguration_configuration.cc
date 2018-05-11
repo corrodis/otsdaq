ec4063b73798b56307e13bfa82828fad9c73b280
rrivera <rrivera@fnal.gov>
2018-05-11 10:50:44 -0500

    Cleaning print outs and commented code.

--- otsdaq-core/ConfigurationPluginDataFormats/IterateConfiguration_configuration.cc
+++ otsdaq-core/ConfigurationPluginDataFormats/IterateConfiguration_configuration.cc
@@ -61,15 +61,15 @@ IterateConfiguration::~IterateConfiguration(void)
 void IterateConfiguration::init(ConfigurationManager *configManager)
 {
 	//do something to validate or refactor table
-	__COUT__ << "*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*" << std::endl;
-	__COUT__ << configManager->__SELF_NODE__ << std::endl;
+//	__COUT__ << "*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*&*" << std::endl;
+//	__COUT__ << configManager->__SELF_NODE__ << std::endl;
 
 	std::string value;
 	auto childrenMap = configManager->__SELF_NODE__.getChildren();
 	for(auto &childPair: childrenMap)
 	{
 		//do something for each row in table
-		__COUT__ << childPair.first << std::endl;
+		//__COUT__ << childPair.first << std::endl;
 		//		__COUT__ << childPair.second.getNode(colNames_.colColumnName_) << std::endl;
 		//childPair.second.getNode(colNames_.colColumnName_	).getValue(value);
 	}
