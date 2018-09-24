6a95d89e9f188c8948b532ac8d2eb202f4705191
Ryan Rivera <rrivera@fnal.gov>
2018-09-24 10:52:28 -0500

    Checkpoint for code editor

--- otsdaq-core/CoreSupervisors/version.h
+++ otsdaq-core/CoreSupervisors/version.h
@@ -10,7 +10,7 @@
 
 #define MYPACKAGE_VERSION_CODE PACKAGE_VERSION_CODE(MYPACKAGE_VERSION_MAJOR, MYPACKAGE_VERSION_MINOR, MYPACKAGE_VERSION_PATCH)
 #ifndef MYPACKAGE_PREVIOUS_VERSIONS
-#define MYPACKAGE_FULL_VERSION_LIST  PACKAGE_VERSION_STRING(MYPACKAGE_VERSION_MAJOR, MYPACKAGE_VERSION_MINOR, MYPACKAGE_VERSION_PATCH)
+#define MYPACKAGE_FULL_VERSION_LIST PACKAGE_VERSION_STRING(MYPACKAGE_VERSION_MAJOR, MYPACKAGE_VERSION_MINOR, MYPACKAGE_VERSION_PATCH)
 #else
 #define MYPACKAGE_FULL_VERSION_LIST MYPACKAGE_PREVIOUS_VERSIONS "," PACKAGE_VERSION_STRING(MYPACKAGE_VERSION_MAJOR, MYPACKAGE_VERSION_MINOR, MYPACKAGE_VERSION_PATCH)
 #endif
@@ -18,12 +18,12 @@
 
 namespace CoreSupervisors
 {
-	const std::string package     = "CoreSupervisors";
-	const std::string versions    = MYPACKAGE_FULL_VERSION_LIST;
-	const std::string summary     = "Core otsdaq supervisor base class";
+	const std::string package = "CoreSupervisors";
+	const std::string versions = MYPACKAGE_FULL_VERSION_LIST;
+	const std::string summary = "Core otsdaq supervisor base class";
 	const std::string description = "Used for basic supervisor functionality including configuration and state transitions.";
-	const std::string authors     = "Ryan Rivera, Lorenzo Uplegger";
-	const std::string link        = "http://xdaq.web.cern.ch";
+	const std::string authors = "Ryan Rivera, Lorenzo Uplegger";
+	const std::string link = "http://xdaq.web.cern.ch";
 	config::PackageInfo getPackageInfo();
 	void checkPackageDependencies() ;
 	std::set<std::string, std::less<std::string> > getPackageDependencies();
