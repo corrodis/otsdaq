a755ef2327ad522e96a3962d70fc9fbfda45a94c
Ryan Rivera <rrivera@fnal.gov>
2020-06-26 12:34:53 -0500

    Fixed Linux OS graphics bug with refresh arrow

--- otsdaq/CoreSupervisors/CorePropertySupervisorBase.cc
+++ otsdaq/CoreSupervisors/CorePropertySupervisorBase.cc
@@ -631,12 +631,12 @@ const std::string& CorePropertySupervisorBase::getTraceLevels()
 			__COUTV__(traceMask.second);
 			//give in 32b chunks since javascript is 32-bit
 			traceLevelsString_ += "," + traceMask.first +
-					",M:" + std::to_string((unsigned int)traceMask.second.M) +
-					":" + std::to_string((unsigned int)(traceMask.second.M>>32)) +
-					",S:" + std::to_string((unsigned int)traceMask.second.S) +
-					":" + std::to_string((unsigned int)(traceMask.second.S>>32)) +
-					",T:" + std::to_string((unsigned int)traceMask.second.T) +
-					":" + std::to_string((unsigned int)(traceMask.second.T>>32));
+					",M:" + std::to_string((unsigned int)(traceMask.second.M>>32)) +
+					":" + std::to_string((unsigned int)traceMask.second.M) +
+					":S:" + std::to_string((unsigned int)(traceMask.second.S>>32)) +
+					":" + std::to_string((unsigned int)traceMask.second.S) +
+					":T:" + std::to_string((unsigned int)(traceMask.second.T>>32)) +
+					":" + std::to_string((unsigned int)traceMask.second.T);
 		}
 
 	}
