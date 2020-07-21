2fdfea34a3ea4e6e74e7c1a0386eb69b214045a0
Kevin <knash201@gmail.com>
2020-07-21 15:35:12 -0500

    Fixed bug in table name of iterator table and improve TCP printouts

--- otsdaq/NetworkUtilities/TCPClientBase.cc
+++ otsdaq/NetworkUtilities/TCPClientBase.cc
@@ -7,6 +7,7 @@
 #include <boost/regex.hpp>
 #include <iostream>
 #include <thread>
+#include "otsdaq/Macros/CoutMacros.h"
 
 using namespace ots;
 
@@ -16,10 +17,10 @@ TCPClientBase::TCPClientBase(const std::string& serverIP, int serverPort) : fSer
 //==============================================================================
 TCPClientBase::~TCPClientBase(void)
 {
-	std::cout << __PRETTY_FUNCTION__ << "Closing TCPSocket #" << getSocketId() << std::endl;
+	__COUT__ << "Closing TCPSocket #" << getSocketId() << std::endl;
 	if(fConnected)
 		close();
-	std::cout << __PRETTY_FUNCTION__ << "TCPSocket #" << getSocketId() << " closed." << std::endl;
+	__COUT__ << "TCPSocket #" << getSocketId() << " closed." << std::endl;
 }
 
 //==============================================================================
@@ -27,15 +28,15 @@ bool TCPClientBase::connect(int retry, unsigned int sleepMilliSeconds)
 {
 	if(fConnected)
 	{
-		std::cout << __PRETTY_FUNCTION__ << "I am already connected...what is going on?" << std::endl;
+		__COUT__ << "I am already connected...what is going on?" << std::endl;
 		throw std::runtime_error(std::string("I am already connected...what is going on?"));
 		abort();
 	}
 
-	std::cout << __PRETTY_FUNCTION__ << "Connecting Client socket to server name-" << fServerIP << "-serverPort: " << fServerPort << std::endl;
+	__COUT__ << "Connecting Client socket to server name-" << fServerIP << "-serverPort: " << fServerPort << std::endl;
 	std::string serverName = fServerIP;
 	resolveServer(fServerIP);
-	std::cout << __PRETTY_FUNCTION__ << "Connecting Client socket to server ip  -" << fServerIP << "-serverPort: " << fServerPort << std::endl;
+	__COUT__ << "Connecting Client socket to server ip  -" << fServerIP << "-serverPort: " << fServerPort << std::endl;
 	int                status = invalidSocketId;
 	struct sockaddr_in serverSocketAddress;
 	serverSocketAddress.sin_family      = AF_INET;
@@ -44,22 +45,22 @@ bool TCPClientBase::connect(int retry, unsigned int sleepMilliSeconds)
 
 	while(!fConnected && (unsigned int)retry-- > 0)
 	{
-		// std::cout << __PRETTY_FUNCTION__ << "Trying to connect" << std::endl;
+		// __COUT__ << "Trying to connect" << std::endl;
 		TCPSocket::open();
 		status = ::connect(getSocketId(), (struct sockaddr*)&serverSocketAddress, sizeof(serverSocketAddress));
-		// std::cout << __PRETTY_FUNCTION__ << "Done Connect" << std::endl;
+		// __COUT__ << "Done Connect" << std::endl;
 		if(status == -1)
 		{
 			if((unsigned int)retry > 0)
 			{
-				std::cout << __PRETTY_FUNCTION__ << "WARNING: Can't connect to " << serverName << ". The server might still be down...Sleeping "
+				__COUT__ << "WARNING: Can't connect to " << serverName << ". The server might still be down...Sleeping "
 				          << sleepMilliSeconds << "ms and then retry " << (unsigned int)retry << " more times." << std::endl;
 				std::this_thread::sleep_for(std::chrono::milliseconds(sleepMilliSeconds));
 				continue;
 			}
 			else
 			{
-				std::cout << __PRETTY_FUNCTION__ << "ERROR: Can't connect to " << serverName << " aborting!" << std::endl;
+				__COUT__ << "ERROR: Can't connect to " << serverName << " aborting!" << std::endl;
 				break;
 			}
 		}
@@ -69,7 +70,7 @@ bool TCPClientBase::connect(int retry, unsigned int sleepMilliSeconds)
 		//			int       socketLength       = 0;
 		//			socklen_t sizeOfSocketLength = sizeof(socketLength);
 		//			status = getsockopt(getSocketId(), SOL_SOCKET, SO_SNDBUF,
-		//&socketLength, &sizeOfSocketLength); 			std::cout << __PRETTY_FUNCTION__
+		//&socketLength, &sizeOfSocketLength); 			__COUT__
 		//<< "TCPConnect sendBufferSize initial: " << socketLength << " status/errno=" <<
 		// status << "/" << errno << " sizeOfSocketLength=" << sizeOfSocketLength <<
 		// std::endl;
@@ -82,14 +83,14 @@ bool TCPClientBase::connect(int retry, unsigned int sleepMilliSeconds)
 		// std::endl ; 			socketLength = 0;
 		//			status = getsockopt(getSocketId(), SOL_SOCKET, SO_SNDBUF,
 		//&socketLength, &sizeOfSocketLength); 			if (socketLength < (sendBufferSize
-		//* 2)) 				std::cout << __PRETTY_FUNCTION__ <<  "sendBufferSize " <<
+		//* 2)) 				__COUT__ <<  "sendBufferSize " <<
 		// socketLength
 		//<< " not expected (" << sendBufferSize << " status/errno=" << status << "/" <<
 		// errno << ")" << std::endl; 			else 				std::cout <<
 		//__PRETTY_FUNCTION__ <<  "sendBufferSize " << socketLength << " status/errno=" <<
 		// status << "/" << errno << std::endl;
 		//		}
-		std::cout << __PRETTY_FUNCTION__ << "Succesfully connected to server " << fServerIP << " port: " << fServerPort << std::endl;
+		__COUT__ << "Succesfully connected to server " << fServerIP << " port: " << fServerPort << std::endl;
 		fConnected = true;
 	}
 
@@ -118,7 +119,7 @@ void TCPClientBase::resolveServer(std::string& serverIP)
 	    "\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)");
 	boost::regex ip_regex(ipv4.c_str());
 
-	// std::cout << __PRETTY_FUNCTION__ << "Checking exp1: " << serverIP << std::endl;
+	// __COUT__ << "Checking exp1: " << serverIP << std::endl;
 	if(boost::regex_match(serverIP, ip_regex))  // It is already in the correct format!
 		return;
 	else if(serverIP == "localhost" || serverIP == "localhost.localdomain")
