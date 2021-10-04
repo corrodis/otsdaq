d9e7051d617f12513e5a6b98a4b16f658ab1ebcb
Eric Flumerfelt <eflumerf@fnal.gov>
2021-10-04 14:36:35 -0500

    More work on ConfigurationInterface plugins

---
+++ otsdaq/Macros/ConfigurationInterfacePluginMacros.h
@@ -0,0 +1,13 @@
+#ifndef _ots_ConfigurationInterfacePluginMacro_h_
+#define _ots_ConfigurationInterfacePluginMacro_h_
+
+#include "otsdaq/ConfigurationInterface/ConfigurationInterface.h"
+
+namespace ots
+{
+typedef ConfigurationInterface*(cbmakeFunc_t)();
+}
+
+#define DEFINE_OTS_CONFIGURATION_INTERFACE(klass) \
+	extern "C" ConfigurationInterface* make() { return new klass(); }
+#endif /* _ots_TablePluginMacro_h_ */
